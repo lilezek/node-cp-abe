3257465a529cdea882b982e366102959b55fe8f3
ezequiel <ezequiel.santamaria@um.es>
2017-05-02 14:03:56 +0200

    Better postbuild script.

--- postbuild.sh
+++ postbuild.sh
@@ -1,2 +1,9 @@
-node_modules/.bin/node-gyp rebuild
+if [ -d node_modules ]
+then
+  NODE_MODULES=node_modules
+else
+  NODE_MODULES=../
+fi
+
+$NODE_MODULES/.bin/node-gyp rebuild
 cp build/Release/cpabe.node .
